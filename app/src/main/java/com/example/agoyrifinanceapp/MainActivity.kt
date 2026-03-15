217076344293cfcc56012ab4a052a72ba12d4fe9
Amir <age81737@lasallebajio.edu.mx>
2026-03-15 08:47:47 -0600

    se hizo el mainactivity y se da por terminado

--- app/src/main/java/com/example/agoyrifinanceapp/MainActivity.kt
+++ app/src/main/java/com/example/agoyrifinanceapp/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.agoyrifinanceapp.Components.HeaderSection
+import com.example.agoyrifinanceapp.Components.SummaryCardsSection
+import com.example.agoyrifinanceapp.Components.TransactionsSection
+import com.example.agoyrifinanceapp.models.currentUser
+import com.example.agoyrifinanceapp.models.recentTransactions
+import com.example.agoyrifinanceapp.models.summaryCards
 import com.example.agoyrifinanceapp.ui.theme.AGoyriFinanceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AGoyriFinanceAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color(0xFFFAFAFA))
+    ) {
+        HeaderSection(user = currentUser)
+        Spacer(modifier = Modifier.height(8.dp))
+        SummaryCardsSection(cards = summaryCards)
+        Spacer(modifier = Modifier.height(16.dp))
+        TransactionsSection(transactions = recentTransactions)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     AGoyriFinanceAppTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
